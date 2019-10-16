7078c0ff213f64fc30ac8b506dfc50562b126ee8
Aatif Fraz <imaatiffraz@gmail.com>
2019-10-16 17:11:28 +0530

    Update BasicTesting.cpp

--- Primality_Testing/BasicTesting.cpp
+++ Primality_Testing/BasicTesting.cpp
@@ -1,10 +1,10 @@
 /*
    By definition a prime number doesn't have any divisors other than 1 and itself.
    Also , for every every number , if a*b == n , one of them is less than or equal to sqrt(n) and other 
-   is greater than equal to sqrt(n). */
+   is greater than equal to sqrt(n). 
 _____________________________________________________________________________________________________
 A funtion to check prime
------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------*/
 bool ifprime(int n){
     bool prime = 1; // Assuming n prime.
     for(int i = 2 ; i*i <= n ; i++){
@@ -15,9 +15,9 @@ bool ifprime(int n){
     }
     return prime;
 }
-_______________________________________________________________________________________________________
+/*_______________________________________________________________________________________________________
 *An extra optimization of the above code is when you are use the fact that except 2,3 , all other primes
  are of the form 6k+1 or 6k+5.
  Hence you check for 2 , 3 then make a loop from i = 5, checking divisibility with i , i + 2 and incrementing
  i by 6 each iteration.
- _____________________________________________________________________________________________________
+ _____________________________________________________________________________________________________*/
